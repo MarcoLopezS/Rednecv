ef39451ac45f217af77bb7cea537680666bb21c5
Marco Lopez <mlopez@marostdevelopers.com>
2015-10-12 11:50:01 -0500

    controller de imagen para modificar el tamaño

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,13 @@
+<?php namespace Cms\Http\Controllers;
+
+class ImageController extends Controller {
+
+    public function adaptiveResize($folder, $width, $height, $image)
+    {
+    	$file = public_path().'/upload/' . $folder . '/' .$image;
+    	$image = \Image::make($file);
+    	$image->fit($width, $height);
+    	return $image->response();
+    }
+
+}
